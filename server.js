e455c6c61725628b1abbc389b7bb8496835ec41b
Akama Aka <akama.aka@akami-solutions.cc>
2024-03-26 21:43:31 +0100

    Refactor error handling routes in server.js
    This revision simplifies error handling routes in the server.js file by replacing individual routes for specific error codes with a single dynamic route. This new approach reduces redundancy and improves the readability of the code. A switch statement is now used to determine the appropriate error page to display based on the status code.
    
    Signed-off-by: SlushUwU <rology@satowa-network.eu>

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ module.exports = async function (fastify, opts) {
     fastify.use('/css/(.*)', serveStatic(path.join(__dirname, '/css')));
     fastify.use('/js', serveStatic(path.join(__dirname, '/js')));
     fastify.get("/akami-cgi/css/:asset", (request, reply) => {
-        let asset = path.basename(req.params.asset);
+        let asset = path.basename(request.params.asset);
         let filePath = path.normalize(path.join(__dirname, '/assets/css/', asset));
         let basePath = path.resolve(__dirname, 'assets/css');
 
@@ -20,82 +20,46 @@ module.exports = async function (fastify, opts) {
         } else {
             const stream = fs.createReadStream(filePath, 'utf8');
             reply.header("Content-Type", "text/css").send(stream || null);
-
-            // ... weiter mit den Datei-Streaming-Code
         }
     })
     fastify.get("/akami-cgi/js/:asset", (request, reply) => {
         const stream = fs.createReadStream(path.join(__dirname, '/assets/js/'+request.params.asset), 'utf8');
         reply.header("Content-Type", "application/javascript").send(stream || null);
     })
-    fastify.get("/akami-cgi/404", (request, reply) => {
-        const stream = fs.readFile(path.join(__dirname, '/html/pageNotFound.html'), 'utf8', function (err, data)  {
+    fastify.get("/akami-cgi/:code", (request, reply) => {
+        let fn;
+        switch (request.params.code) {
+            case "404":
+                fn = "pageNotFound.html";
+                break;
+            case "500":
+                fn = "internalServerError.html"
+                break;
+            case "502":
+                fn = "badGateway.html"
+                break;
+            case "503":
+                fn = "serviceUnavailable.html";
+                break;
+            case "504":
+                fn = "gatewayTimeout.html"
+                break;
+            case "1000":
+                fn = "directAccessForbidden.html"
+                break;
+            case "1001":
+                fn = "noDatabaseConnection.html";
+                break;
+            default:
+                fn = null;
+        }
+        fs.readFile(path.join(__dirname, `/html/${fn}`), 'utf-8', (err, data) => {
             if(err) {
-                return console.log(err);
+                console.error(err);
             }
-            var result = data.replace("__implement-ray-id__", request.headers["cf-ray"] || request.id);
-            var result = result.replace("__implement-ip__", request.headers["cf-ip"] || request.ip);
+            const res = data.replace("__implement-ray-id__", request.headers["cf-ray"] || request.id);
+            const result = res.replace("__implement-ip__", request.headers["cf-ip"] || request.ip);
             reply.header("Content-Type", "text/html").send(result);
-        })
-    })
-    fastify.get("/akami-cgi/502", (request, reply) => {
-        const stream = fs.readFile(path.join(__dirname, '/html/badGateway.html'), 'utf8', function (err, data)  {
-            if(err) {
-                return console.log(err);
-            }
-            var result = data.replace("__implement-ray-id__", request.headers["cf-ray"] || request.id);
-            var result = result.replace("__implement-ip__", request.headers["cf-ip"] || request.ip);
-            reply.header("Content-Type", "text/html").send(result);
-        })
-    })
-    fastify.get("/akami-cgi/1000", (request, reply) => {
-        const stream = fs.readFile(path.join(__dirname, '/html/directAccessForbidden.html'), 'utf8', function (err, data)  {
-            if(err) {
-                return console.log(err);
-            }
-            var result = data.replace("__implement-ray-id__", request.headers["cf-ray"] || request.id);
-            var result = result.replace("__implement-ip__", request.headers["cf-ip"] || request.ip);
-            reply.header("Content-Type", "text/html").send(result);
-        })
-    })
-    fastify.get("/akami-cgi/504", (request, reply) => {
-        const stream = fs.readFile(path.join(__dirname, '/html/gatewayTimeout.html'), 'utf8', function (err, data)  {
-            if(err) {
-                return console.log(err);
-            }
-            var result = data.replace("__implement-ray-id__", request.headers["cf-ray"] || request.id);
-            var result = result.replace("__implement-ip__", request.headers["cf-ip"] || request.ip);
-            reply.header("Content-Type", "text/html").send(result);
-        })
-    })
-    fastify.get("/akami-cgi/500", (request, reply) => {
-        const stream = fs.readFile(path.join(__dirname, '/html/internalServerError.html'), 'utf8', function (err, data)  {
-            if(err) {
-                return console.log(err);
-            }
-            var result = data.replace("__implement-ray-id__", request.headers["cf-ray"] || request.id);
-            var result = result.replace("__implement-ip__", request.headers["cf-ip"] || request.ip);
-            reply.header("Content-Type", "text/html").send(result);
-        })
-    })
-    fastify.get("/akami-cgi/1001", (request, reply) => {
-        const stream = fs.readFile(path.join(__dirname, '/html/noDatabaseConnection.html'), 'utf8', function (err, data)  {
-            if(err) {
-                return console.log(err);
-            }
-            var result = data.replace("__implement-ray-id__", request.headers["cf-ray"] || request.id);
-            var result = result.replace("__implement-ip__", request.headers["cf-ip"] || request.ip);
-            reply.header("Content-Type", "text/html").send(result);
-        })
-    })
-    fastify.get("/akami-cgi/503", (request, reply) => {
-        const stream = fs.readFile(path.join(__dirname, '/html/serviceUnavailable.html'), 'utf8', function (err, data)  {
-            if(err) {
-                return console.log(err);
-            }
-            var result = data.replace("__implement-ray-id__", request.headers["cf-ray"] || request.id);
-            var result = result.replace("__implement-ip__", request.headers["cf-ip"] || request.ip);
-            reply.header("Content-Type", "text/html").send(result);
-        })
+        });
     })
 }
