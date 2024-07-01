6202f02a6e166389143d7a65cb688367b0ff2afb
Akama Aka <akama.aka@akami-solutions.cc>
2024-07-01 12:46:28 +0200

    Refactor request IP and ID retrievals in server handling
    This commit updates the server.js to improve the way it retrieves request IPs and IDs. Instead of fetching this information in several lines of code across the functions, the refactor adopts a more efficient, streamlined approach by assigning these values to variables and reusing them. This change reduces code redundancy and enhances readability.
    
    Signed-off-by: Akama Aka <akama.aka@akami-solutions.cc>

--- server.js
+++ server.js
@@ -60,6 +60,8 @@ module.exports = async function (fastify, opts) {
     })
     fastify.get(`/${path_name}/status/:code`, (req, rep) => {
         let fn;
+        let req_id = req.headers["cf-ray"] || req.headers["cdn-requestid"] || req.headers["X-Amz-Cf-Id"] || req.headers["akamai-x-get-request-id"] || req.headers["x-appengine-request-log-id"] || req.headers["requestId"] || req.headers["opc-request-id"] || req.id;
+        let req_ip = req.headers["Cf-Connecting-Ip"] || req.headers["cf-connecting-ipv6"] || req.headers["x-real-ip"] || req.headers["x-forwarded-for"] || req.ip;
         switch (req.params.code) {
             case "403":
                 fn = "accessForbidden.html";
@@ -96,7 +98,7 @@ module.exports = async function (fastify, opts) {
                 console.error(err);
             }
             // Inject the CloudFlare Ray ID
-            let res = data.replace("__implement-ray-id__", req.headers["cf-ray"] || req.headers["cdn-requestid"] || req.headers["X-Amz-Cf-Id"] || req.headers["akamai-x-get-request-id"] || req.headers["x-appengine-request-log-id"] || req.headers["requestId"] || req.headers["opc-request-id"] || req.id);
+            let res = data.replace("__implement-ray-id__", req_id);
             // Inject the JavaScript Sources at the bottom of the Body
             res = res.replace('__implement_body_script__', '<!--Implement Body Scripts--><script src="/'+path_name+'/js/bootstrap.bundle.min.js"></script>');
             // Inject the Styling
@@ -104,31 +106,27 @@ module.exports = async function (fastify, opts) {
                 '    <link rel="stylesheet" href="/'+path_name+'/css/style.css">')
 
             // Inject the Client IP
-            const result = res.replace("__implement-ip__", req.headers["cf-ip"] || req.ip);
-            rep.headers({
-                "Content-Type": "text/html",
-                "Server": "Akami Solutions",
-                "X-Powered-By": "A DNS System by Akami Solutions"
-            }).send(result);
+            const result = res.replace("__implement-ip__", req_ip);
             rep.headers(defaultHeader).send(result);
         });
     })
     fastify.get(`/${path_name}/xray`, async (req, rep) => {
         // Resolve the IP Country
         const options = {method: 'GET', headers: {'User-Agent': 'insomnia/8.3.0'}};
-        let ip = req.headers["Cf-Connecting-Ip"] || req.headers["x-forwarded-for"] || req.headers["cf-pseudo-ipv4"] || req.ip;
+        let req_id = req.headers["cf-ray"] || req.headers["cdn-requestid"] || req.headers["X-Amz-Cf-Id"] || req.headers["akamai-x-get-request-id"] || req.headers["x-appengine-request-log-id"] || req.headers["requestId"] || req.headers["opc-request-id"] || req.id;
+        let req_ip = req.headers["Cf-Connecting-Ip"] || req.headers["cf-connecting-ipv6"] || req.headers["x-real-ip"] || req.headers["x-forwarded-for"] || req.ip;
         let ttl = 7884000000; // 3 Monate
-        let val = await getCache(ip);
+        let val = await getCache(req_ip);
         const rayResponse = function (response) {
-            return `Host=${req.headers[":authority"] || req.headers["host"]}\nrequest=${req.headers["cf-ray"] || req.headers["cdn-requestid"] || req.id}\nip=${ip}\ncountry=${response["data"]["located_resources"][0]["locations"][0]["country"]} # IP resolution by RIPE DB & MaxMind`
+            return `Host=${req.headers[":authority"] || req.headers["host"]}\nrequest=${req_id}\nip=${req_ip}\ncountry=${response["data"]["located_resources"][0]["locations"][0]["country"]} # IP resolution by RIPE DB & MaxMind`
         }
-        const rayResponseLocal = `Host=${req.headers["host"]}\nRequest ID: ${req.headers["cf-ray"] || req.headers["cdn-requestid"] || req.id}\nIP: ${ip}\nCountry: Local IP`;
+        const rayResponseLocal = `Host=${req.headers["host"]}\nRequest ID: ${req_id}\nIP: ${req_ip}\nCountry: Local IP`;
         if (!val) {
-            return await fetch('https://stat.ripe.net/data/maxmind-geo-lite/data.json?resource=' + ip, options)
+            return await fetch('https://stat.ripe.net/data/maxmind-geo-lite/data.json?resource=' + req_ip, options)
                 .then(response => response.json())
                 .then(response => {
                     if (response && response["data"]["located_resources"].length > 0) {
-                        return setCache(ip, response, ttl).then((r) => {
+                        return setCache(req_ip, response, ttl).then((r) => {
                             if (r.error) {
                                 fastify.log.error(r.error);
                                 return rep.headers("Content-Type", "text/plain").code(500).send("Internal Server Error");
@@ -139,7 +137,7 @@ module.exports = async function (fastify, opts) {
                             return rep.headers("Content-Type", "text/plain").code(500).send("Internal Server Error");
                         });
                     } else {
-                        return setCache(ip, response, ttl).then((r) => {
+                        return setCache(req_ip, response, ttl).then((r) => {
                             if (r.error) {
                                 fastify.log.error(r.error);
                             }
